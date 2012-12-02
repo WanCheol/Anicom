c72e896a9e5dcce9798cb2ea391e6ff5ff7fbdc8
choidahyun <uhoo1009@hanmail.net>
2012-12-02 18:48:26 +0900

    modify sql file

--- WebContent/hospitals.sql
+++ WebContent/hospitals.sql
@@ -12,7 +12,8 @@ CREATE TABLE hospitals (
 	name VARCHAR(20) not null,
 	phone VARCHAR(20) NOT NULL, 
 	address VARCHAR(100) not null,
-	grade varchar(20) not null
+	grade int,
+	guest varchar(20) not null
 );
 
 INSERT INTO hospitals VALUES ('hos1','123','woori','031-657-2911','seoul gangseo mokdong 231-1bungi','3');
